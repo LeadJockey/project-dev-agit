751f7cc079d016d0980406d5d0264f2e2263cf00
leadjocky <dltldnjs5790@naver.com>
2019-02-25 13:58:31 +0900

    UPdate test

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
-
 import Counter from './Counter'
+import { a } from 'util/index.js'
+console.log('uitl import test', a());
 
 class App extends Component {
   render() {
