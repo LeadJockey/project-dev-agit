b248f508045328bb6976e23d4a7a89be0c8a0474
hatemodify <hatemodify@gmail.com>
2019-02-26 16:47:17 +0900

    login 인증 테스트

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,36 @@
 import React, { Component } from 'react'
-import { BrowserRouter as Router, Link } from "react-router-dom"
+import { BrowserRouter as Router, Link } from 'react-router-dom'
 
 import Routes from 'routes/Routes'
 
+import GoogleLogin from 'react-google-login'
 
 require('assets/scss/app.scss')
 
 class App extends Component {
-  render() {
+  responseGoogle (response) {
+    console.log(response.w3)
+
+    localStorage.v = 0
+  }
+  render () {
     return (
       <Router>
-        <div className="wrapper">
+        <div className='wrapper'>
           <div>Header</div>
           <div>Index</div>
-          <div className="main">
-            <Link to="/home" >home</Link>
+          <div className='main'>
+            <Link to='/home'>home</Link>
             <Routes />
           </div>
+
+          <GoogleLogin
+            clientId='558582794007-m5unl35bhe57kfdtrmebi4ek50bunpp4.apps.googleusercontent.com'
+            buttonText='Login'
+            onSuccess={this.responseGoogle}
+            onFailure={this.responseGoogle}
+          />
+
           <div>Footer</div>
         </div>
       </Router>
