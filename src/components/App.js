b0b38b65dd5cdafb680e407840b925c428061167
hatemodify <hatemodify@gmail.com>
2019-02-27 21:13:22 +0900

    style setting

--- src/components/App.js
+++ src/components/App.js
@@ -4,19 +4,27 @@ import { BrowserRouter as Router, Link } from 'react-router-dom'
 import { SizeContext } from 'util/context'
 import Routes from 'routes/Routes'
 import Login from './Login'
-import Popup from './Popup';
+import Popup from './Popup'
 import 'assets/scss/app.scss'
 
-
-
 class App extends Component {
-  render() {
+  render () {
     return (
       <Router>
-        <SizeContext.Provider value={{ width: window.innerWidth, height: window.innerHeight }}>
+        <SizeContext.Provider
+          value={{ width: window.innerWidth, height: window.innerHeight }}
+        >
           <div className='wrapper'>
-            <div>Header</div>
-            <div>Index</div>
+            <header className='app_header'>header</header>
+            <aside className='side_menu'>
+              <div className='user_profile' />
+              <ul className='user_menu'>
+                <li>User Menu 1</li>
+                <li>User Menu 2</li>
+                <li>User Menu 3</li>
+                <li>User Menu 4</li>
+              </ul>
+            </aside>
             <Login />
             <div className='main'>
               <Link to='/home'>home</Link>
