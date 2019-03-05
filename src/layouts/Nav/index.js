a25027509bb73465c5406b9e2a352ef2baa02c48
leadjocky <dltldnjs5790@naver.com>
2019-03-05 18:21:35 +0900

    UPDATE NAV

--- src/layouts/Nav/index.js
+++ src/layouts/Nav/index.js
@@ -1,9 +1,22 @@
 import React, { Component } from 'react'
+import { Link } from "react-router-dom"
+import { ROUTE_PATH } from 'util/constants'
+import './index.scss'
 
 class Nav extends Component {
   render() {
     return (
-      <nav className="app_nav"></nav>
+      <nav className="app_nav">
+        <strong>ROUTES</strong>
+        <hr />
+        <ul className="list_route">
+          <li><Link to={ROUTE_PATH.INDEX} >INDEX</Link></li>
+          <li><Link to={ROUTE_PATH.HOME} >HOME</Link></li>
+          <li><Link to={ROUTE_PATH.PROJECTS +'/1'} >PROJECTS 1</Link></li>
+          <li><Link to={ROUTE_PATH.WORKS + '/1'} >WORKS 1</Link></li>
+          <li><Link to={ROUTE_PATH.COUNTER} >COUNTER</Link></li>
+        </ul>
+      </nav>
     )
   }
 }
