1c41ac895cc90fcd5d4b64b21c3988c39eba5bab
leadjocky <dltldnjs5790@naver.com>
2019-03-08 20:29:18 +0900

    UPDATE D3 - TODO re build with axis, scale, selection

--- src/routes/D3.js
+++ src/routes/D3.js
@@ -4,74 +4,88 @@ import { korean } from 'languages'
 import * as d3 from "d3";
 
 class D3 extends Component {
-
+  //TODO yarn add d3-axis d3-interpolate d3-scale d3-selection https://medium.com/@caspg/responsive-chart-with-react-and-d3v4-afd717e57583
   // dataset = [4, 8, 15, 16, 23, 42, 59, 64, 71, 83, 90, 95]
-  dataset = [4, 8, 15]
+  dataset = [110, 900, 130, 140, 150, 810, 1500, 160, 732, 900]
 
   componentDidMount() {
 
-    // const targets = d3
-    //   .select('.target')
-    //   .selectAll('p')
-    //   .data(this.dataset)
-    //   .text((fontSize) => `font-size:${fontSize}px`)
-
-    // targets
-    //   .enter()
-    //   .append('p')
-    //   .text((fontSize) => `font-size:${fontSize}px`)
-    //   .transition()
-    //   .duration(2000)
-    //   .delay((fontSize, idx) => ((fontSize + idx) * 10))
-    //   .style('font-size', (fontSize) => `${fontSize}px`)
-    //   .style('color', () => `hsl(${Math.random() * 360}, 100%, 50%)`)
-
-    // targets
-    //   .exit()
-    //   .remove()
-
-    const SVG_WIDTH = 600
-    const SVG_HEIGHT = 300
-    const SVG_BAR_PADDING = 10
     const SVG_NOT_SUPPORT_MSG = 'Sorry, your browser does not support inline SVG.'
-    const DATA = [10, 20, 30, 40, 50, 110, 120, 130, 140, 150, 1110, 1120, 1130, 1140, 1150]
+    const SVG_BAR_PADDING = 10
+    const DATA_DIVISION = 5
+    const SVG_HEIGHT = 300
+    const SVG_WIDTH = 600
+    const DATA = this.dataset
     const DATA_LENGTH = DATA.length
-    const DATA_DIVISION = 5 // 5%
 
     const svg = d3
       .select('.target')
       .append('svg')
-      .attr('width', SVG_WIDTH)
+      .attr('width', 800)
       .attr('height', SVG_HEIGHT)
-      .style('background-color', () => '#fff')
       .text(() => SVG_NOT_SUPPORT_MSG)
 
     svg
+      .selectAll('g')
+      .data(['_d3_rect_bar', '_d3_line_x'])
+      .enter()
+      .append('g')
+      .attr('class', (className) => className)
+      .transition()
+      .duration(2000)
+      .delay((d, i) => ((d + i) * 10))
+
+    svg
+      .selectAll('._d3_line_x')
+      .selectAll('line')
+      .data([SVG_HEIGHT / 3 * 1, SVG_HEIGHT / 3 * 2, SVG_HEIGHT / 3 * 3])
+      .enter()
+      .append('line')
+      .attr('x1', () => 0)
+      .attr('y1', (x) => x)
+      .attr('x2', () => SVG_WIDTH)
+      .attr('y2', (x) => x)
+      .attr('fill', () => '#efefef')
+      .attr("stroke", "#efefef")
+      .attr("stroke-width", .5)
+
+    svg
+      .selectAll('._d3_rect_bar')
       .selectAll('rect')
       .data(DATA)
       .enter()
       .append('rect')
-      .transition()
-      .delay(() => 750)
-      .attr('x', (d, i) => i * (SVG_BAR_PADDING + SVG_WIDTH) / DATA_LENGTH)
+      
+      
+
+      .attr('x', (d, i) => i * SVG_WIDTH / DATA_LENGTH)
       .attr('y', (d) => SVG_HEIGHT - parseInt(d / DATA_DIVISION, 10))
       .attr('width', () => (SVG_WIDTH / DATA_LENGTH) - SVG_BAR_PADDING)
       .attr('height', (d, i) => parseInt(d / DATA_DIVISION, 10))
       .attr('fill', () => `hsl(${Math.random() * 360}, 100%, 50%)`)
+      .on("mouseover", (d, i) => {
+        svg
+          .append("text")
+          .attr("class", "_hover")
+          .text(() => d)
+          .attr('x', () => 700)
+          .attr('y', () => 150)
+          .attr('fill', '#fff')
+          .attr('text-anchor', 'middle')
+          .attr('fill', '#fff')
+      })
+      .on("mouseout", () => { d3.selectAll("._hover").remove() })
 
-    svg.selectAll('g')
-      .data(DATA)
-      .enter()
-      .append('text')
-      .transition()
-      .delay(() => 250)
+    // svg.selectAll('._d3_rect_bar').selectAll('rect').transition(t)
+
   }
+
 
   render() {
     return (
       <section className="app_section">
         <TitleBox {...korean.route.d3} />
-        <div className="target" />
+        <div className="target" style={{ padding: '20px' }} />
       </section>
     )
   }
