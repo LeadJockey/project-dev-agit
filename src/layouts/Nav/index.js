92c2c836bab13869bb17b0521072e35bba52803a
hatemodify <hatemodify@gmail.com>
2019-03-08 15:12:33 +0900

    update style & layout modified

--- src/layouts/Nav/index.js
+++ src/layouts/Nav/index.js
@@ -1,23 +1,58 @@
 import React, { Component } from 'react'
-import { Link } from "react-router-dom"
-import { ROUTE_PATH } from 'util/constants'
+import { Link } from 'react-router-dom'
+import { ROUTE_PATH, ROUTE_ICON } from 'util/constants'
 import './index.scss'
 
 class Nav extends Component {
-  render() {
+  render () {
     return (
-      <nav className="app_nav">
-        <strong>ROUTES</strong>
-        <hr />
-        <ul className="list_route">
-          <li><Link to={ROUTE_PATH.INDEX} >INDEX</Link></li>
-          <li><Link to={ROUTE_PATH.HOME} >HOME</Link></li>
-          <li><Link to={ROUTE_PATH.USERS} >USERS</Link></li>
-          <li><Link to={ROUTE_PATH.NEWS} >NEWS</Link></li>
-          <li><Link to={ROUTE_PATH.PROJECTS} >PROJECTS</Link></li>
-          <li><Link to={ROUTE_PATH.WORKS} >WORKS</Link></li>
-          <li><Link to={ROUTE_PATH.TEST} >TEST</Link></li>
-          <li><Link to={ROUTE_PATH.D3} >D3</Link></li>
+      <nav className='app_nav'>
+        <ul className='list_route'>
+          <li>
+            <Link to={ROUTE_PATH.INDEX}>INDEX</Link>
+          </li>
+          <li>
+            <Link to={ROUTE_PATH.HOME}>
+              <i className={ROUTE_ICON.HOME} />
+              HOME
+            </Link>
+          </li>
+          <li>
+            <Link to={ROUTE_PATH.USERS}>
+              <i className={ROUTE_ICON.USERS} />
+              USERS
+            </Link>
+          </li>
+          <li>
+            <Link to={ROUTE_PATH.NEWS}>
+              <i className={ROUTE_ICON.NEWS} />
+              NEWS
+            </Link>
+          </li>
+          <li>
+            <Link to={ROUTE_PATH.PROJECTS}>
+              <i className={ROUTE_ICON.PROJECTS} />
+              PROJECTS
+            </Link>
+          </li>
+          <li>
+            <Link to={ROUTE_PATH.WORKS}>
+              <i className={ROUTE_ICON.WORKS} />
+              WORKS
+            </Link>
+          </li>
+          <li>
+            <Link to={ROUTE_PATH.TEST}>
+              <i className={ROUTE_ICON.TEST} />
+              TEST
+            </Link>
+          </li>
+          <li>
+            <Link to={ROUTE_PATH.D3}>
+              <i className={ROUTE_ICON.D3} />
+              D3
+            </Link>
+          </li>
         </ul>
       </nav>
     )
--- src/util/constants.js
+++ src/util/constants.js
@@ -1,5 +1,6 @@
 // google authentication key
-export const CLIENT_ID = '558582794007-m5unl35bhe57kfdtrmebi4ek50bunpp4.apps.googleusercontent.com'
+export const CLIENT_ID =
+  '558582794007-m5unl35bhe57kfdtrmebi4ek50bunpp4.apps.googleusercontent.com'
 // static route path
 export const ROUTE_PATH = {
   INDEX: '/',
@@ -9,7 +10,16 @@ export const ROUTE_PATH = {
   PROJECTS: '/projects',
   WORKS: '/works',
   TEST: '/test',
-  D3: '/d3',
+  D3: '/d3'
+}
+export const ROUTE_ICON = {
+  HOME: 'ti-home',
+  USERS: 'ti-user',
+  NEWS: 'ti-world',
+  PROJECTS: 'ti-package',
+  WORKS: 'ti-target',
+  TEST: 'ti-rocket',
+  D3: 'ti-bar-chart'
 }
 // animate frame per sec
-export const FPS = 1000 / 60 
+export const FPS = 1000 / 60
